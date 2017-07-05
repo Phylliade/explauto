9022308511827d62288e9387292dcb0a91a0e941
Pierre Manceron <pierre.manceron@gmail.com>
2017-07-05 16:04:24 +0200

    Add full support for local config

--- tests/config.py
+++ tests/config.py
@@ -4,3 +4,10 @@ n_goals = 1000
 n_goals_test = 100
 n_motor_commands = 100
 test_set_size = 100
+
+
+local_variables = ["n_goals", "n_goals_test", "n_motor_commands", "test_set_size"]
+# Overwrite variables with the ones provided in config_local
+variables = locals()
+for variable in local_variables:
+    variables[variable] = getattr(config_local, variable, variables[variable])
