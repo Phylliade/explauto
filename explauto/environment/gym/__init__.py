89bfaf6de53358b1bfff5c61a3961444efb8f441
Pierre Manceron <pierre.manceron@gmail.com>
2017-07-05 11:44:02 +0200

    Add energy in the descriptor space

--- explauto/environment/gym/__init__.py
+++ explauto/environment/gym/__init__.py
@@ -7,7 +7,7 @@ def maximal_span_mountain_car(rollout):
     # A rollout = Series of (position, speed)
     min_span = min(rollout[:, 0] - (-0.523))
     max_span = max(rollout[:, 0] - (-0.523))
-    return((min_span, max_span))
+    return([min_span, max_span])
 
 
 def maximal_height_mountain_car(rollout):
@@ -26,13 +26,31 @@ def maximal_position_mountain_car(rollout):
     return([maximum_position])
 
 
+def energy_mountain_car(rollout):
+    """Computes the energy consumed during a rollout"""
+    # Use the energy formula as given by:
+    # https://github.com/openai/gym/blob/master/gym/envs/classic_control/continuous_mountain_car.py#L72
+    action = rollout[:, 1]
+    ernegy = np.sum(np.power(action, 2) * 0.1)
+    return([ernegy])
+
+
+def maximal_position_energy_mountain_car(rollout):
+    """Use the ernegy and the maximal position"""
+    energy = energy_mountain_car(rollout)[0]
+    max_pos = maximal_position_mountain_car(rollout)[0]
+
+    return([max_pos, energy])
+
 environment = GymEnvironment
 configurations = {
     'MCC_default': {"name": "MountainCarContinuous-v0", "observation_function": maximal_span_mountain_car, "s_mins": [-1.2, -1.2], "s_maxs": [0.6, 0.6]},
     'MCC_span': {"name": "MountainCarContinuous-v0", "observation_function": maximal_span_mountain_car, "s_mins": [-1.2, -1.2], "s_maxs": [0.6, 0.6]},
     'MCC_height': {"name": "MountainCarContinuous-v0", "observation_function": maximal_height_mountain_car, "s_mins": [0], "s_maxs": [1.2]},
     'MCC_min_pos': {"name": "MountainCarContinuous-v0", "observation_function": minimal_position_mountain_car, "s_mins": [-1.2], "s_maxs": [0.]},
-    'MCC_max_pos': {"name": "MountainCarContinuous-v0", "observation_function": maximal_position_mountain_car, "s_mins": [0.], "s_maxs": [1.2]}
+    'MCC_max_pos': {"name": "MountainCarContinuous-v0", "observation_function": maximal_position_mountain_car, "s_mins": [0.], "s_maxs": [1.2]},
+    'MCC_max_pos_energy': {"name": "MountainCarContinuous-v0", "observation_function": maximal_position_energy_mountain_car, "s_mins": [0., 0.], "s_maxs": [1.2, 100]}
+    # Maximal energy = 1 * 1000 * 0.1 = 100
 }
 
 
